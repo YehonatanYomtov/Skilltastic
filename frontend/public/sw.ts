874fb4080e71fb34d783e7978fce7bbb663e528a
Yehonatan <y.yomtov8@gmail.com>
2024-09-30 15:26:13 +0300

    fixed sw.ts file type error.

--- frontend/public/sw.ts
+++ frontend/public/sw.ts
@@ -1,9 +1,11 @@
+/// <reference lib="webworker" />
+
 const CACHE_NAME = "static-v1";
 const URLS_TO_CACHE = ["/", "/index.html", "/manifest.json"];
 
 // Install event: Cache important files
-self.addEventListener("install", (e: ExtendableEvent) => {
-  e.waitUntil(
+self.addEventListener("install", (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(URLS_TO_CACHE);
     })
@@ -11,8 +13,8 @@ self.addEventListener("install", (e: ExtendableEvent) => {
 });
 
 // Activate event: Clean up old caches
-self.addEventListener("activate", (e: ExtendableEvent) => {
-  e.waitUntil(
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
@@ -26,10 +28,10 @@ self.addEventListener("activate", (e: ExtendableEvent) => {
 });
 
 // Fetch event: Serve cached content or fetch from network
-self.addEventListener("fetch", (e: FetchEvent) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
